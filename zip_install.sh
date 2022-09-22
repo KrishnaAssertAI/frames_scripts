9e9609b5aca36dadb295654a8a2c6d7747dcbda6
Krishna Prasad <97428524+KrishnaAssertAI@users.noreply.github.com>
2022-09-22 21:00:08 +0530

    Update zip_install.sh

--- zip_install.sh
+++ zip_install.sh
@@ -1,2 +1,2 @@
 pip3 install google-api-python-client google-auth-httplib2 google-auth-oauthlib
-zip -r /home/nvidia/frames_scripts/frames.zip frames/*
+zip -rq /home/nvidia/frames_scripts/frames.zip frames/*
